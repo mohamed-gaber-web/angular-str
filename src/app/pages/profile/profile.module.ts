4e77733152fe792dcf91ec29092292d0321f6f0a
mohamed gaber <mgaberweb@gmail.com>
2023-08-22 00:19:16 +0300

    fix some issue

--- src/app/pages/profile/profile.module.ts
+++ src/app/pages/profile/profile.module.ts
@@ -5,12 +5,14 @@ import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './component/profile.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
 
 
 @NgModule({
   declarations: [
     ProfileComponent,
-    ResetPasswordComponent
+    ResetPasswordComponent,
+    EditProfileComponent
   ],
   imports: [
     CommonModule,
